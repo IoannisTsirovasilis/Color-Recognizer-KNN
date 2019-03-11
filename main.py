3a2f8722f81f8f00b54605224e87cbf42b99fc60
GiannisTsirovasilis <tsirovasilis.y@caredirect.com>
2019-03-11 16:30:11 +0200

    - Added Text File With Colors

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 import numpy as np
 from cv2 import VideoCapture, imshow, waitKey, destroyAllWindows
 
-# BGR
-BLACK = [0, 0, 0]
-WHITE = [255, 255, 255]
-LIGHT_GRAY = [224, 224, 224]
-GRAY = [128, 128, 128]
-DARK_GRAY = [64, 64, 64]
-RED = [0, 0, 255]
-PINK = [208, 96, 255]
-PURPLE = [255, 32, 160]
-LIGHT_BLUE = [255, 208, 80]
-BLUE = [255, 32, 0]
-YELLOW_GREEN = [128, 255, 96]
-GREEN = [0, 192, 0]
-YELLOW = [32, 224, 255]
-ORANGE = [16, 160, 255]
-BROWN = [96, 128, 160]
-PALE_PINK = [160, 208, 255]
-CYAN = [255, 255, 0]
+COLORS = []
+LABELS = []
+with open("colors.txt", "r") as f:
+    colors = f.readlines()
 
-COLORS = np.array([BLACK, WHITE, LIGHT_GRAY, GRAY, DARK_GRAY, RED, PINK, PURPLE, LIGHT_BLUE, BLUE, YELLOW_GREEN,
-                   GREEN, YELLOW, ORANGE, BROWN, PALE_PINK, CYAN])
-LABELS = np.array(["Black", "White", "Light Gray", "Gray", "Dark Gray", "Red", "Pink", "Purple", "Light Blue",
-                   "Blue", "Yellow Green", "Green", "Yellow", "Orange", "Brown", "Pale Pink", "Cyan"])
+for c in colors:
+    tmp = np.asarray(c.split(','))
+    color = np.asarray([int(x) for x in tmp[:-1]])
+    label = tmp[-1].replace('\n', '')
+    LABELS.append(label)
+    COLORS.append(color)
 
+COLORS = np.asarray(COLORS)
 cap = VideoCapture(0)
-print (cap.isOpened())
+print(cap.isOpened())
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
